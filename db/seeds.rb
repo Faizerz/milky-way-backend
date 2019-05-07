e3e5c84c6dd75973f8af1429846bf9704ed868d6
github username <github email address>
2019-05-07 14:44:03 +0100

    fixed typos

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@ milky_way = SolarSystem.create(name: 'Milky Way')
 
 mercury = Planet.create(name: 'Mercury', solar_system_id: milky_way.id, diameter: 4878, mass: 0.06, orbital_period: 88, day_length: 4222.6, gravity: 3.7, sun_distance: 57.9, temperature: 167, atmosphere: 0)
 venus = Planet.create(name: 'Venus', solar_system_id: milky_way.id, diameter: 12104, mass: 0.82, orbital_period: 225, day_length: 2802.0, gravity: 8.9, sun_distance: 108.2, temperature: 464, atmosphere: 92)
-earth = Planet.create(name: 'Earth', solar_system_id: milky_way.id, diameter: 12756, mass: 1.00, orbital_period: 365, day_length: 24.0, gravity: 9.8, sun_distance: 149.6, temperature: -15, atmosphere: 1)
+earth = Planet.create(name: 'Earth', solar_system_id: milky_way.id, diameter: 12756, mass: 1.00, orbital_period: 365, day_length: 24.0, gravity: 9.8, sun_distance: 149.6, temperature: 15, atmosphere: 1)
 mars = Planet.create(name: 'Mars', solar_system_id: milky_way.id, diameter: 6792, mass: 0.11, orbital_period: 687, day_length: 708.7, gravity: 3.7, sun_distance: 227.9, temperature: -65, atmosphere: 0.01)
 jupiter = Planet.create(name: 'Jupiter', solar_system_id: milky_way.id, diameter: 142984, mass: 317.89, orbital_period: 4331, day_length: 9.9, gravity: 23.1, sun_distance: 778.6, temperature: -110, atmosphere: 0)
 saturn = Planet.create(name: 'Saturn', solar_system_id: milky_way.id, diameter: 120536, mass: 95.17, orbital_period: 10747, day_length: 10.7, gravity: 9.8, sun_distance: 1433.5, temperature: -140, atmosphere: 0)
